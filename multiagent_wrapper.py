7b488a79239f2bcbc03ec5eb3521b2a79e8a13da
Sebastian Griesbach <sebastian.griesbach@zoho.eu>
2021-04-10 15:14:55 +0200

    added typing to multi agent wrapper

--- multiagent_wrapper.py
+++ multiagent_wrapper.py
@@ -1,18 +1,18 @@
 import gym
 import numpy as np
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Tuple, Dict, Union
 
 class Multiagent_wrapper(ABC, gym.Wrapper):
-    def __init__(self, env, state_space, num_agents, action_spaces, observation_spaces):
+    def __init__(self, env: gym.Env, state_space: gym.Space, num_agents: int, action_spaces: List[gym.Space], observation_spaces: List[gym.Space]) -> None:
         """initalizes Multiagent wrapper
 
         Args:
-            env (gym.env): underlying gym environment
-            state_space ([type]): Observation space of the underlying environment
-            num_agents ([type]): number of agents participating in this environment
-            action_spaces ([type]): List of action spaces of agents. According to order of agents.
-            observation_spaces ([type]): List of observation spaces of agents. Accoding to order of agents.
+            env (gym.Env): underlying gym environment
+            state_space (gym.Space): Observation space of the underlying environment
+            num_agents (int): number of agents participating in this environment
+            action_spaces (List[gym.Space]): List of action spaces of agents. According to order of agents.
+            observation_spaces (List[gym.Space]): List of observation spaces of agents. Accoding to order of agents.
         """
         gym.Wrapper.__init__(self, env)
 
@@ -21,14 +21,14 @@ class Multiagent_wrapper(ABC, gym.Wrapper):
         self.action_spaces = action_spaces
         self.observation_spaces = observation_spaces
 
-    def step(self, actions):
+    def step(self, actions: List[np.array]) -> Tuple[np.array, List[np.array], List[float], List[bool], Union[None, Dict]]:
         """Performe one timestep
 
         Args:
-            actions ([type]): List of actions to performe. According to order of agents.
+            actions (List[numpy.array]): List of actions to performe. According to order of agents.
 
         Returns:
-            [type]: Tuple containing (state, observations, rewards, done, info) where observations and rewards are list according to the order of agents.
+            Tuple[numpy.array, List[numpy.array], List[float], List[bool], Union[None, Dict]]: Tuple containing (state, observations, rewards, done, info) where observations and rewards are list according to the order of agents.
         """
         joint_action = self._build_joint_action(actions)
         state, reward, done, info = self.env.step(joint_action)
@@ -36,34 +36,34 @@ class Multiagent_wrapper(ABC, gym.Wrapper):
         rewards = self._build_rewards(state, reward, info)
         return state, observations, rewards, done, info
 
-    def reset(self):
+    def reset(self) -> Tuple[np.array, List[np.array]]:
         """Reset the environment
 
         Returns:
-            [type]: Tuple containing (state. observations) where observations is a list of observations according to the order of agents.
+            Tuple[np.array, List[np.array]]: Tuple containing (state. observations) where observations is a list of observations according to the order of agents.
         """
         state = self.env.reset()
         observations = self._build_observations(state)
         return state, observations
 
     @abstractmethod
-    def _build_joint_action(self, actions) -> np.array:
+    def _build_joint_action(self, actions: List[np.array]) -> np.array:
         """This function needs to merge the actions together such that the underlying environment can process them.
 
         Args:
-            actions ([type]): List of actions according to the order of agents.
+            actions (List[numpy.array]): List of actions according to the order of agents.
 
         Returns:
-            np.array: merged actions such that the underlying environment can process them.
+            numpy.array: merged actions such that the underlying environment can process them.
         """
         pass
 
     @abstractmethod
-    def _build_observations(self, state) -> List[np.array]:
+    def _build_observations(self, state: np.array) -> List[np.array]:
         """This function needs to split up the state into the observations of the single agents.
 
         Args:
-            state ([type]): total state of the environment
+            state (numpy.array): total state of the environment
 
         Returns:
             List[numpy.array]: List of observations according to order of agents.
@@ -71,15 +71,15 @@ class Multiagent_wrapper(ABC, gym.Wrapper):
         pass
 
     @abstractmethod
-    def _build_rewards(self, state, reward, info) -> list:
+    def _build_rewards(self, state: np.array, reward: float, info: Union[None, Dict]) -> List[float]:
         """This function needs to determin the rewards for all agents
 
         Args:
-            state ([type]): Total state of the underlying environment
-            reward ([type]): reward of the underlying environment
-            info ([type]): info of the underlying environment
+            state (numpy.array): Total state of the underlying environment
+            reward (float): reward of the underlying environment
+            info (Union[None, Dict])): info of the underlying environment
 
         Returns:
-            list: List of rewards according to order of agents
+            List[float]: List of rewards according to order of agents
         """
         pass
