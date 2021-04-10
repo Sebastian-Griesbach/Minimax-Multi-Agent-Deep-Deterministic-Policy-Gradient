30a69a7208b9978bfbac6ea6c3b8a9bd98069032
Sebastian Griesbach <sebastian.griesbach@zoho.eu>
2021-04-10 15:16:30 +0200

    added typing to multiagent critic

--- multiagent_critic.py
+++ multiagent_critic.py
@@ -1,5 +1,6 @@
 import torch
 from abc import ABC, abstractmethod
+from typing import List
 
 class Multiagent_critic(ABC, torch.nn.Module):
     def __init__(self):
@@ -8,12 +9,12 @@ class Multiagent_critic(ABC, torch.nn.Module):
         super(Multiagent_critic, self).__init__()
 
     @abstractmethod
-    def forward(self, state, actions) -> torch.tensor:
+    def forward(self, state: torch.tensor, actions: List[torch.tensor]) -> torch.tensor:
         """This method needs to forward the state and the actions through the models such that they have a pytorch traceable gradient.
 
         Args:
-            state ([type]): Total state of the underlying environment
-            actions ([type]): List of actions according to the order of agents
+            state (torch.tensor): Total state of the underlying environment
+            actions (List[torch.tensor]): List of actions according to the order of agents
 
         Returns:
             torch.tensor: The critics evalutaion of the Q-value of this state action pair for a specific agent.
